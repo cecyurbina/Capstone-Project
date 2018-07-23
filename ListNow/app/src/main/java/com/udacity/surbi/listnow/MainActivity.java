2dae04629a1fe51c708b60e0b1df3b35169c9f0f
Cecy Urbina <ceciliaurbina29@gmail.com>
2018-07-22 20:12:11 -0500

    nav drawer done

--- ListNow/app/src/main/java/com/udacity/surbi/listnow/MainActivity.java
+++ ListNow/app/src/main/java/com/udacity/surbi/listnow/MainActivity.java
@@ -76,21 +76,14 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
-
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
+        switch (id){
+            case R.id.nav_new_list:
+                break;
+            case R.id.nav_search:
+                break;
+            case R.id.nav_logout:
+                break;
         }
-
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
