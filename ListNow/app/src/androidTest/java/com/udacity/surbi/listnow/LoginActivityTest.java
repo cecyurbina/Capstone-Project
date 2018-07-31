243ce91b28873dad63e7d204eb3ef3ad9e7d8fc5
Cecy Urbina <ceciliaurbina29@gmail.com>
2018-07-31 01:37:02 -0500

    delete unnecessary imports

--- ListNow/app/src/androidTest/java/com/udacity/surbi/listnow/LoginActivityTest.java
+++ ListNow/app/src/androidTest/java/com/udacity/surbi/listnow/LoginActivityTest.java
@@ -1,37 +1,19 @@
 package com.udacity.surbi.listnow;
 
-import android.content.ComponentName;
-import android.content.Context;
-import android.content.Intent;
-import android.support.test.InstrumentationRegistry;
-import android.support.test.espresso.contrib.RecyclerViewActions;
 import android.support.test.espresso.intent.rule.IntentsTestRule;
 import android.support.test.filters.LargeTest;
-import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
-import android.support.v7.widget.RecyclerView;
 
 
 import com.udacity.surbi.listnow.activity.LoginActivity;
 import com.udacity.surbi.listnow.activity.MainActivity;
-import com.udacity.surbi.listnow.activity.NewListActivity;
 
-import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static android.support.test.espresso.Espresso.onView;
-import static android.support.test.espresso.action.ViewActions.click;
-import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.intent.Intents.intended;
-import static android.support.test.espresso.intent.matcher.ComponentNameMatchers.hasShortClassName;
 import static android.support.test.espresso.intent.matcher.IntentMatchers.hasComponent;
-import static android.support.test.espresso.intent.matcher.IntentMatchers.toPackage;
-import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static android.support.test.espresso.matcher.ViewMatchers.withId;
-import static com.udacity.surbi.listnow.fragment.ListHomeFragment.KEY_LIST_JSON;
-import static org.hamcrest.core.AllOf.allOf;
 
 /*
 Test phone view opens an activity to see steps details and tablet view shows details on same view
--- ListNow/app/src/main/java/com/udacity/surbi/listnow/ListNow.java
+++ ListNow/app/src/main/java/com/udacity/surbi/listnow/ListNow.java
@@ -2,8 +2,6 @@ package com.udacity.surbi.listnow;
 
 import android.app.Application;
 
-import com.google.firebase.database.FirebaseDatabase;
-
 public class ListNow extends Application {
 
     @Override
